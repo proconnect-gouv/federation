6554be838e0220586b656d7932c39401fdc8c50f
Hugues Charleux <hcx@uhuru.io>
2020-04-23 16:55:52 +0200

    [FC-73]🚚 Rename 'ConsentManagement' into 'Consent'

--- libs/consent-management/src/consent-management.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConsentManagementService } from './consent-management.service';
-
-@Module({
-  providers: [ConsentManagementService],
-  exports: [ConsentManagementService],
-})
-export class ConsentManagementModule {}
