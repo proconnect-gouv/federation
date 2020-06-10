c8c998cd5b44a1984510dc72c3d09a0195d5d593
Hugues Charleux <hcx@uhuru.io>
2020-06-10 18:18:07 +0200

    Remove mongoose depreciation warning

--- libs/mongoose/src/mongoose.provider.ts
+++ libs/mongoose/src/mongoose.provider.ts
@@ -1,9 +1,15 @@
 /* istanbul ignore file */
 
 // Declarative code
+import * as mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@fc/config';
 import { MongooseConfig } from './dto';
+
+/**
+ * Prevent depreciation warning
+ */
+mongoose.set('useCreateIndex', true);
 
 export const mongooseProvider = MongooseModule.forRootAsync({
   imports: [ConfigModule],
