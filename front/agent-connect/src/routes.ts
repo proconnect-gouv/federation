15bd963048e996f8e5a5ec88866f17043e5f3633
manufao <emaravilha@kaliopmail.com>
2020-12-01 09:37:12 +0000

    [FC-223] :art: improvement of react routes to be compatible with production

--- front/agent-connect/src/routes.ts
+++ front/agent-connect/src/routes.ts
@@ -8,17 +8,14 @@ const routes = [
     component: ErrorPage,
     exact: true,
     id: 'error-page',
-    path: '/error',
+    path: '/api/v2/error',
     title: "Erreur lors de l'authentification",
   },
   {
     component: Homepage,
     exact: true,
     id: 'homepage',
-    /* @TODO ETQ Ops, je peux déployer une image docker d'AgentConnect
-     * Pouvoir faire cohabiter l'url '/interaction/:interactionId' (docker) et '/api/v2/interaction/:interactionId' (prod)
-     * @see https://gitlab.dev-franceconnect.fr/france-connect/fc/-/issues/223 */
-    path: '/interaction/:interactionId',
+    path: '/api/v2/interaction/:interactionId',
     title: '',
   },
 ];
