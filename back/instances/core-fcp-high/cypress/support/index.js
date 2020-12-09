3954392e024355188497b3f25cf7f165a78f4d24
knabeth@kaliopmail.com <knabeth@kaliopmail.com>
2020-12-09 15:34:48 +0100

    [FC-292] add e2e test for tab navigation

--- back/instances/core-fcp-high/cypress/support/index.js
+++ back/instances/core-fcp-high/cypress/support/index.js
@@ -4,6 +4,7 @@
  * @see https://on.cypress.io/custom-commands
  */
 import { hasBusinessLog, hasError } from './commands';
+import "cypress-plugin-tab";
 
 Cypress.Commands.add('hasError', hasError);
 Cypress.Commands.add('hasBusinessLog', hasBusinessLog);
