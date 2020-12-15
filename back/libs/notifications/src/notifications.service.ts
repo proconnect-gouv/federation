9a46c093283302c87022653a871c6ac48b3d9b20
knabeth@kaliopmail.com <knabeth@kaliopmail.com>
2020-12-15 18:21:13 +0100

    [FC-293] add info message on mire

---
+++ back/libs/notifications/src/notifications.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { INotifications } from './interfaces';
+import { validateDto, asyncFilter } from '@fc/common';
+import { validationOptions } from '@fc/config';
+import { NotificationsDto } from './dto';
+import { LoggerService } from '@fc/logger';
+
+@Injectable()
+export class NotificationsService {
+  constructor(
+    @InjectModel('notifications')
+    private readonly notificationsModel,
+  ) {}
+
+  private async findActiveNotifications(): Promise<INotifications> {
+
+    const result = await this.notificationsModel
+      .findOne(
+        {
+          isActive: true,
+        },
+        {
+          _id: false,
+          message: true,
+        },
+      )
+      .exec();
+
+    return result;
+  }
+  async getNotifications(): Promise<string> {
+      const notifications: any = await this.findActiveNotifications();
+    return notifications;
+  }
+}
