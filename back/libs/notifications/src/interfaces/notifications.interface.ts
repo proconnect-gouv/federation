6f2ba255fd7e0ff2579af8e63f603647c03f82dc
knabeth@kaliopmail.com <knabeth@kaliopmail.com>
2020-12-23 17:20:29 +0100

    [FC-292-293] remove unused things

--- back/libs/notifications/src/interfaces/notifications.interface.ts
+++ back/libs/notifications/src/interfaces/notifications.interface.ts
@@ -1,5 +1,5 @@
 import { Document } from 'mongoose';
 
 export interface INotifications extends Document {
-  message: string
+  message: string;
 }
--- back/libs/notifications/src/schemas/notifications.schema.ts
+++ back/libs/notifications/src/schemas/notifications.schema.ts
@@ -8,7 +8,6 @@ export const NotificationsSchema = new mongoose.Schema(
     isActive: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date },
-  
   },
   {
     // Mongoose add 's' at the end of the collection name without this argument
