fdc03000a9634ff508a20e5e8191fce5a65d63dc
Stéphane Herman <sherman@kaliop.com>
2021-01-07 15:47:06 +0100

    [FC-300] Add E2E test that check unautorized scopes

--- back/instances/core-fcp-high/cypress/integration/9.0.service-provider.spec.js
+++ back/instances/core-fcp-high/cypress/integration/9.0.service-provider.spec.js
@@ -29,6 +29,30 @@ describe('Service Provider', () => {
     cy.hasError('Y030106');
   });
 
+  it('should return to SP1 with an error "invalid_scope" if the requested scope contains unhautorized scopes for this FS', () => {
+    const url = getAuthorizeUrl({
+      // oidc param
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      client_id: 'my-service-provider-only-openid-birthdate-scopes',
+    });
+    cy.visit(url, {
+      failOnStatusCode: false,
+    });
+
+    cy.url()
+      .should(
+        'contains',
+        `${Cypress.env(`SP1_ROOT_URL`)}/oidc-callback/envIssuer`,
+      )
+      .should('contains', 'error=invalid_scope')
+      .should(
+        'contains',
+        'error_description=requested%20scope%20is%20not%20whitelisted',
+      )
+      .should('contains', 'scope=gender')
+      .should('contains', 'state=stateTraces');
+  });
+
   it('should trigger error Y030118 if the parameter redirect_uri does NOT match one of the redirect uris of the SP in database', () => {
     const url = getAuthorizeUrl({
       // oidc param
