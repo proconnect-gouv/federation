5ad011e33e57da71da06e84436ff7ee9a7238cf9
manufao <emaravilha@kaliopmail.com>
2021-02-03 11:35:35 +0000

    [FC-231-E2E] ETQ Exploitant, je veux que les modifications sur les FI soient prise en comptes automatiquement

--- back/instances/core-fca-high/cypress/support/commands/index.js
+++ back/instances/core-fca-high/cypress/support/commands/index.js
@@ -2,3 +2,5 @@ export * from './has-business-log.command';
 export * from './has-error.command';
 export * from './console.command';
 export * from './cookie.command';
+export * from './e2e.command';
+export * from './reset-db.command';
