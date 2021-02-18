74e7adc375376b9f99e088c94987abccab4bbca3
Hugues Charleux <h@chrx.io>
2021-02-18 10:03:36 +0000

    Resolve "ETQ core, Je gère sans erreur le cas où featureHandlers n'est pas un objet"

---
+++ back/libs/feature-handler/src/validators/registred-feature-handler.validator.spec.ts
@@ -0,0 +1,73 @@
+import { IsRegisteredFeatureHandlerConstraint } from './registered-feature-handler.validator';
+import { FeatureHandler } from '../decorators';
+
+describe('IsRegisteredFeatureHandler', () => {
+  describe('IsRegisteredFeatureHandlerConstraint', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+      jest.restoreAllMocks();
+    });
+
+    it('should return false if input is undefined', () => {
+      // Given
+      const input = undefined;
+      const validator = new IsRegisteredFeatureHandlerConstraint();
+      // When
+      const result = validator.validate(input);
+      // Then
+      expect(result).toBeFalsy();
+    });
+
+    it('should return false if input is a string', () => {
+      // Given
+      const input = ('some String' as unknown) as any;
+      const validator = new IsRegisteredFeatureHandlerConstraint();
+      // When
+      const result = validator.validate(input);
+      // Then
+      expect(result).toBeFalsy();
+    });
+
+    it('should return false if input is a number', () => {
+      // Given
+      const input = (42 as unknown) as any;
+      const validator = new IsRegisteredFeatureHandlerConstraint();
+      // When
+      const result = validator.validate(input);
+      // Then
+      expect(result).toBeFalsy();
+    });
+
+    it('should return true if input is an object with registred handler', () => {
+      // Given
+      const registredHandler = 'myRegistredHandler';
+      const askedHandler = registredHandler;
+      jest
+        .spyOn(FeatureHandler, 'getAll')
+        .mockImplementationOnce(() => [registredHandler]);
+
+      const input = { bar: askedHandler };
+      const validator = new IsRegisteredFeatureHandlerConstraint();
+      // When
+      const result = validator.validate(input);
+      // Then
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false if input is an object without registred handler', () => {
+      // Given
+      const registredHandler = 'myRegistredHandler';
+      const askedHandler = 'anotherHandler';
+      jest
+        .spyOn(FeatureHandler, 'getAll')
+        .mockImplementationOnce(() => [registredHandler]);
+
+      const input = { bar: askedHandler };
+      const validator = new IsRegisteredFeatureHandlerConstraint();
+      // When
+      const result = validator.validate(input);
+      // Then
+      expect(result).toBeFalsy();
+    });
+  });
+});
