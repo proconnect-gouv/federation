c44eac3ed2fcb98085f0f5a8c46066fa8734235a
Benjamin Roullet <broullet@kaliopmail.com>
2021-02-25 11:14:52 +0000

    Fc 376 ETQ Utilisateur, je ne peux pas utiliser des FI présents dans la liste noire d'un FS

--- back/libs/oidc-client/src/oidc-client.controller.ts
+++ back/libs/oidc-client/src/oidc-client.controller.ts
@@ -86,8 +86,9 @@ export class OidcClientController {
      * This is a specific behaviour for FC and not for fsp*v2
      */
     let serviceProviderId: string | null;
+    const { interactionId } = req.fc;
     try {
-      const { spId } = await this.session.get('spId');
+      const { spId } = await this.session.get(interactionId);
       serviceProviderId = spId;
     } catch (error) {
       serviceProviderId = null;
@@ -140,7 +141,8 @@ export class OidcClientController {
      */
     let serviceProviderId: string | null;
     try {
-      const { spId } = await this.session.get('spId');
+      const { interactionId } = req.fc;
+      const { spId } = await this.session.get(interactionId);
       serviceProviderId = spId;
     } catch (error) {
       serviceProviderId = null;
