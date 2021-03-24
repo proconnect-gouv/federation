2efea10d1683f0fb707d3dceea9e89aba6369867
Matthieu Lassalvy <sixertoy.github@gmail.com>
2021-03-24 17:58:08 +0000

    [FC-332] add connect notification email template
    - copy from core-legacy
    - update nestcli.json assets

--- back/apps/core-fcp/src/handlers/email/core-fcp-send-email.handler.ts
+++ back/apps/core-fcp/src/handlers/email/core-fcp-send-email.handler.ts
@@ -1,11 +1,18 @@
+import * as ejs from 'ejs';
 import { Injectable } from '@nestjs/common';
 import { LoggerService } from '@fc/logger';
 import { SessionService } from '@fc/session';
 import { ConfigService, validationOptions } from '@fc/config';
-import { MailerConfig, MailerService, MailTo } from '@fc/mailer';
+import {
+  MailerConfig,
+  MailerService,
+  MailTo,
+  NoEmailException,
+  MailerNotificationConnectException,
+  ConnectNotificationEmailParameters,
+} from '@fc/mailer';
 import { IFeatureHandler, FeatureHandler } from '@fc/feature-handler';
 import { validateDto } from '@fc/common';
-import { CoreFcpNoEmailException } from '../../exceptions';
 
 @Injectable()
 @FeatureHandler('core-fcp-send-email')
@@ -20,6 +27,59 @@ export class CoreFcpSendEmailHandler implements IFeatureHandler {
   ) {
     this.logger.setContext(this.constructor.name);
     this.configMailer = this.config.get<MailerConfig>('Mailer');
+  }
+
+  getTodayFormattedDate(datejs: Date): string {
+    const timeZone = 'Europe/Paris';
+    const locateDate = datejs.toLocaleString('fr-Fr', { timeZone });
+    const date = new Date(locateDate);
+
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = `${date.getHours()}`.padStart(2, '0');
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+
+    const formattedDate = `Le ${day}/${month}/${year} à ${hours}:${minutes}`;
+    return formattedDate;
+  }
+
+  hydrateConnectNotificationEmailTemplate(
+    connectNotificationEmailParameters: ConnectNotificationEmailParameters,
+  ): string {
+    const { template } = this.configMailer;
+    const htmlContent = ejs.render(template, {
+      locals: connectNotificationEmailParameters,
+    });
+    return htmlContent;
+  }
+
+  async getConnectNotificationEmailBodyContent(req: any): Promise<string> {
+    const { interactionId } = req.fc;
+    const session = await this.session.get(interactionId);
+    const { idpName, spIdentity, spName } = session;
+    const today = this.getTodayFormattedDate(new Date());
+    const connectNotificationEmailParameters = {
+      familyName: spIdentity.family_name,
+      givenName: spIdentity.given_name,
+      idpName,
+      spName,
+      today,
+    };
+
+    const dtoValidationErrors = await validateDto(
+      connectNotificationEmailParameters,
+      ConnectNotificationEmailParameters,
+      validationOptions,
+    );
+    if (dtoValidationErrors.length > 0) {
+      throw new MailerNotificationConnectException();
+    }
+
+    const htmlContent = this.hydrateConnectNotificationEmailTemplate(
+      connectNotificationEmailParameters,
+    );
+    return htmlContent;
   }
 
   /**
@@ -37,9 +97,7 @@ export class CoreFcpSendEmailHandler implements IFeatureHandler {
 
     const { from } = this.configMailer;
     const { interactionId } = req.fc;
-    const { spName, idpName, spIdentity } = await this.session.get(
-      interactionId,
-    );
+    const { spName, spIdentity } = await this.session.get(interactionId);
     const {
       email,
       given_name: givenName,
@@ -55,14 +113,15 @@ export class CoreFcpSendEmailHandler implements IFeatureHandler {
     const errors = await validateDto(mailTo, MailTo, validationOptions);
 
     if (errors.length > 0) {
-      throw new CoreFcpNoEmailException('No email defined');
+      throw new NoEmailException();
     }
 
+    const body = await this.getConnectNotificationEmailBodyContent(req);
     this.mailer.send({
       from,
       to,
       subject: `Connexion depuis FranceConnect sur ${spName}`,
-      body: `Connexion établie via ${idpName} !`,
+      body,
     });
   }
 }
