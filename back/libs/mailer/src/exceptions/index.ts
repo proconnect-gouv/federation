75d664f8148e48d9cee518c10ec82f9540d1ff64
Matthieu Lassalvy <sixertoy.github@gmail.com>
2021-03-24 17:58:08 +0000

    [FC-332] add connect notification email template
    - copy from core-legacy
    - update nestcli.json assets

---
+++ back/libs/mailer/src/exceptions/index.ts
@@ -0,0 +1,2 @@
+export * from './no-email.exception';
+export * from './mailer-notification-connect.exception';
---
+++ back/libs/mailer/src/exceptions/no-email.exception.ts
@@ -0,0 +1,10 @@
+import { Description } from '@fc/error';
+import { MailerBaseException } from './mailer-base.exception';
+
+@Description(
+  `Un utilisateur s'est connecté à FranceConnect, il recoit un mail lui notifiant une nouvelle connection à un de ses comptes. Si l'email de cet utilisateur n'est pas présent cette exception sera levée`,
+)
+export class NoEmailException extends MailerBaseException {
+  code = 1;
+  message = 'No email defined';
+}
