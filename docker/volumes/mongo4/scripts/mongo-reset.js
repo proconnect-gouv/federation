24b54f9d2d3d02368b9adb06dbcbfc15e76f0add
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2021-04-06 13:02:42 +0000

    [FC-461] ETQ DEV je n'ai besoin que d'un seul repo pour ma stack FC+ (partie 2)

---
+++ docker/volumes/mongo4/scripts/mongo-reset.js
@@ -0,0 +1,18 @@
+db = db.getSiblingDB("corev2");
+
+print("Reseting account collection...");
+db.account.remove({});
+
+print("Reseting user collection...");
+db.user.remove({});
+
+print("Reseting client collection");
+db.client.remove({});
+
+print("Reseting provider collection");
+db.provider.remove({});
+
+print("Reseting scopelabels collection");
+db.scopelabels.remove({});
+
+print("All collections reseted!");
