1e790f1d2f9099eacf62cb447eeabc05bc4dc8ac
Arnaud Palin Sainte Agathe <apalin@kaliopmail.com>
2021-05-19 10:12:33 +0000

    [FC-520] ETQ ops je dois chiffrer le trafic entre l'application et le RP

--- back/libs/config/src/config.service.ts
+++ back/libs/config/src/config.service.ts
@@ -1,15 +1,18 @@
-import { Inject, Injectable } from '@nestjs/common';
 import {
-  validate,
   ValidatorOptions,
   isString,
   isObject,
+  validateSync,
 } from 'class-validator';
 import * as deepFreeze from 'deep-freeze';
 import { plainToClass } from 'class-transformer';
+import { Inject, Injectable } from '@nestjs/common';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+import { HttpsOptionDto } from '@fc/common';
 import { CONFIG_OPTIONS } from './tokens';
 import { IConfigOptions } from './interfaces';
 import { UnknownConfigurationNameError } from './errors';
+import { readFileSync } from 'fs';
 
 export const validationOptions: ValidatorOptions = {
   skipMissingProperties: false,
@@ -25,15 +28,15 @@ export class ConfigService {
   constructor(@Inject(CONFIG_OPTIONS) configOptions: IConfigOptions) {
     const { config, schema } = configOptions;
 
-    this.validate(config, schema);
+    ConfigService.validate(config, schema);
 
     // No one should be able to override configuration after startup
     this.configuration = deepFreeze(config);
   }
 
-  private async validate(config, schema) {
+  private static validate(config, schema) {
     const object = plainToClass(schema, config);
-    const errors = await validate(object, validationOptions);
+    const errors = validateSync(object, validationOptions);
 
     if (errors.length > 0) {
       console.error('Invalid configuration Error');
@@ -64,5 +67,46 @@ export class ConfigService {
         `Asked unknown configuration: <${next}>`,
       );
     }, this.configuration);
+  }
+
+  /**
+   * @todo #534 ETQ Dev, je veux que la configuration soit initialisé avant l'application Server
+   * cette fonction disparaitra et les options seront intégrés au config standard
+   */
+  /**
+   * Grab the https options from environment variables
+   * @returns {HttpsOptions} the HTTPS definition for NestJS
+   */
+  static getHttpsOptions(): HttpsOptions | null {
+    const {
+      App_HTTPS_SERVER_KEY: keyPath,
+      App_HTTPS_SERVER_CERT: certPath,
+    } = process.env;
+
+    const hasHttpsOptions = keyPath && certPath;
+
+    if (!hasHttpsOptions) {
+      return null;
+    }
+
+    const key = ConfigService.getHttpsCert(keyPath);
+    const cert = ConfigService.getHttpsCert(certPath);
+
+    const data = {
+      key,
+      cert,
+    };
+
+    ConfigService.validate(data, HttpsOptionDto);
+
+    return data;
+  }
+
+  private static getHttpsCert(path: string): string {
+    try {
+      return readFileSync(path, 'utf-8');
+    } catch (e) {
+      throw new Error(`No HTTPS Certificate found at path : ${path}`);
+    }
   }
 }
