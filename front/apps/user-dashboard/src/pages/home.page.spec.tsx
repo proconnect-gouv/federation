72cca9f536730bed42d88967ee038d46828712ce
Matthieu Lassalvy <sixertoy.github@gmail.com>
2021-06-22 13:16:25 +0000

    #530 Resolve "ETQ DEV j'ai besoin d'un lien entre app back et app front"

---
+++ front/apps/user-dashboard/src/pages/home.page.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Homepage from './home.page';
+
+describe('Homepage', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it('should render something', () => {
+    // setup
+    const { getByRole } = render(<Homepage />);
+    // action
+    const formElement = getByRole('form');
+    // expect
+    expect(formElement).toBeInTheDocument();
+    expect(formElement).toHaveAttribute(
+      'action',
+      'https://ud.docker.dev-franceconnect.fr/api/redirect-to-idp'
+    );
+  });
+});
