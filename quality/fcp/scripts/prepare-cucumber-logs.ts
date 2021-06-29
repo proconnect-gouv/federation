f93515d3c086b4e06100b252282810b12dbc3355
Nicolas Legeay <nlegeay@kaliopmail.com>
2021-06-29 15:44:39 +0200

    [FC-516] Use functions in prepare script

--- quality/fcp/scripts/prepare-cucumber-logs.ts
+++ quality/fcp/scripts/prepare-cucumber-logs.ts
@@ -1,9 +1,14 @@
 /**
  * Add Screenshot and Video attachments to the Cucumber logs
  */
+
 import * as fs from 'fs';
 import * as path from 'path';
 
+import {
+  IJsonFeature,
+  IJsonStep,
+} from '@cucumber/cucumber/lib/formatter/json_formatter';
 import * as glob from 'glob';
 
 // Fetch Arguments
@@ -16,34 +21,42 @@ const [
 ] = process.argv;
 
 // Init
-
-const cukeMap = {};
-const jsonNames = {};
-
-const jsonPath = path.join(__dirname, '..', cucumberJsonDir);
+const cucumberMap: { [key: string]: IJsonFeature } = {};
+const cucumberFileMap: { [key: string]: string } = {};
+const cucumberDirPath = path.join(__dirname, '..', cucumberJsonDir);
 const screenshotsPath = path.join(__dirname, '..', screenshotsDir);
 const videosPath = path.join(__dirname, '..', videosDir);
 
 // Functions
 
 /**
- * Returns the failed step or null
+ * Retrieves the first failed step inside a scenario
+ * @param {IJsonFeature} cuke feature based on Cucumber Json schema
+ * @param {string} scenarioName name of the scenario
+ * @returns {IJsonStep | undefined} the failed step or null
  */
-const getFailedStepFromScenario = (cuke, scenarioName) => {
-  const myScenario = cuke.elements.find((e) => e.name === scenarioName);
+const getFailedStepFromScenario = (
+  cuke: IJsonFeature,
+  scenarioName: string,
+): IJsonStep | undefined => {
+  const myScenario = cuke.elements.find(
+    (e) => e.name.replace(/"/g, '') === scenarioName,
+  );
   if (!myScenario) {
-    return null;
+    return undefined;
   }
   const myStep = myScenario.steps.find(
     (step) => step.result.status !== 'passed',
   );
-  if (myStep && !myStep.embeddings) {
-    myStep.embeddings = [];
-  }
   return myStep;
 };
 
-const addScreenshotToStep = (screenshotPath, step) => {
+/**
+ * Add screenshot link to the step
+ * @param {string} screenshotPath absolute path to the screenshot
+ * @param {IJsonStep} step step to add the screenshot
+ */
+const addScreenshotToStep = (screenshotPath: string, step: IJsonStep): void => {
   const regExp = new RegExp(`^(?:.*${screenshotsDir})(.*/)([^/]*)$`);
   const found = regExp.exec(screenshotPath);
   if (!found) {
@@ -53,10 +66,18 @@ const addScreenshotToStep = (screenshotPath, step) => {
   const encodedPath = `../assets/screenshots${encodeURI(screenshotFolder)}\
 ${encodeURIComponent(screenshotName)}`;
   const screenshotImage = `<img class="screenshot" src="${encodedPath}" />`;
+  if (!step.embeddings) {
+    step.embeddings = [];
+  }
   step.embeddings.push({ data: screenshotImage, mime_type: 'text/html' });
 };
 
-const videoContainer = (videoPath) => {
+/**
+ * Provides a HTML container with the video
+ * @param {string} videoPath absolute path to the video
+ * @returns {string} html content with link to the video
+ */
+const videoContainer = (videoPath: string): string => {
   const videoHTML = `
   <a href="#video" data-toggle="collapse" class="collapsed">+ Show Video</a>
   <div id="video" class="scenario-step-collapse collapse" aria-expanded="false" style="height: 0px;">
@@ -69,61 +90,120 @@ const videoContainer = (videoPath) => {
   return videoHTML;
 };
 
-// Retrieve all the CucumberJson files
-const files = fs
-  .readdirSync(jsonPath, { withFileTypes: true })
-  .filter((file) => file.isFile())
-  .map((file) => file.name);
+// Main functions
 
-// Keep track for each feature of the CucumbeJson file path and content
-files.forEach((file) => {
-  const json = JSON.parse(
-    fs.readFileSync(path.join(jsonPath, file)).toString(),
-  );
-  const arr = file.split('.');
-  const featureName = `${arr[0]}.feature`;
-  jsonNames[featureName] = file;
-  cukeMap[featureName] = json;
-});
+/**
+ * Reads the Cucumber Json files and updates the maps
+ * @param {string} cukeDirPath path to the folder containing the cucumber jon files
+ * @param {Object} cukeFileMap map linking the feature to the cucumber json file name
+ * @param {Object} cukeMap map linking the feature to the cucumber json content
+ */
+const readCucumberFiles = (
+  cukeDirPath: string,
+  cukeFileMap: { [key: string]: string },
+  cukeMap: { [key: string]: IJsonFeature },
+): void => {
+  // For all cucumber json files
+  fs.readdirSync(cukeDirPath, { withFileTypes: true })
+    .filter((file) => file.isFile())
+    .map((file) => file.name)
+    .forEach((fileName) => {
+      // For each feature
+      const arr = fileName.split('.');
+      const featureName = `${arr[0]}.feature`;
+      const json = JSON.parse(
+        fs.readFileSync(path.join(cukeDirPath, fileName)).toString(),
+      );
+      // Keep the file name
+      cukeFileMap[featureName] = fileName;
+      // Keep the cucumber json content
+      // The IJsonFeature object is in one array of one element
+      cukeMap[featureName] = json[0];
+    });
+};
 
-// For each Cypress video (local execution)
-const videos = glob.sync(`${videosPath}/**/*.mp4`);
-videos.forEach((videoPath) => {
-  const featureName = path.basename(videoPath, '.mp4');
-  // Add video link in the feature description
-  const videoLink = videoContainer(videoPath);
-  const featureDescription = `${cukeMap[featureName][0].description}\n<br />${videoLink}`;
-  cukeMap[featureName][0].description = featureDescription;
-});
-
-// Retrieve all Cypress screenshots
-const screenshots = glob
-  .sync(`${screenshotsPath}/**/*.png`)
-  .map((screenshotPath) => {
-    return {
-      feature: path.basename(path.dirname(screenshotPath)),
-      screenshotName: path.basename(screenshotPath),
-      screenshotPath,
-    };
+/**
+ * Add the video links to the Cucumber Json
+ * @param {string} videosDirPath path to the folder containing the Cypress videos
+ * @param {Object} cukeMap map linking the feature to the cucumber json content
+ */
+const addVideoLinks = (
+  videosDirPath: string,
+  cukeMap: { [key: string]: IJsonFeature },
+): void => {
+  // For each Cypress video (local execution)
+  glob.sync(`${videosDirPath}/**/*.mp4`).forEach((videoPath) => {
+    const featureName = path.basename(videoPath, '.mp4');
+    if (cukeMap[featureName]) {
+      // Append the description with the video link
+      const videoLink = videoContainer(videoPath);
+      const featureDescription = `${cukeMap[featureName].description}\n<br />${videoLink}`;
+      cukeMap[featureName].description = featureDescription;
+    }
   });
+};
 
-// For each Cypress screenshot
-screenshots.forEach(({ feature, screenshotName, screenshotPath }) => {
-  const scenarioName = screenshotName
-    .match(/--\s(.+).png/)[1]
-    .replace('(failed)', '')
-    .replace(/\(example #\d+\)/, '')
-    .trim();
+/**
+ * Add the screenshot links to the Cucumber Json
+ * @param {string} screenshotsDirPath path to the folder containing the Cypress screenshots
+ * @param {Object} cukeMap map linking the feature to the cucumber json content
+ */
+const addScreenshotLinks = (
+  screenshotsDirPath: string,
+  cukeMap: { [key: string]: IJsonFeature },
+): void => {
+  // For each Cypress screenshot
+  glob
+    .sync(`${screenshotsDirPath}/**/*.png`)
+    .map((screenshotPath) => {
+      return {
+        feature: path.basename(path.dirname(screenshotPath)),
+        screenshotName: path.basename(screenshotPath),
+        screenshotPath,
+      };
+    })
+    .forEach(({ feature, screenshotName, screenshotPath }) => {
+      if (!cukeMap[feature]) {
+        return;
+      }
+      const scenarioName = screenshotName
+        .match(/--\s(.+).png/)[1]
+        .replace('(failed)', '')
+        .replace(/\(example #\d+\)/, '')
+        .trim();
 
-  // Add the screenshot to the failed step
-  const myStep = getFailedStepFromScenario(cukeMap[feature][0], scenarioName);
-  if (myStep) {
-    addScreenshotToStep(screenshotPath, myStep);
+      // Add the screenshot to the failed step
+      const myStep = getFailedStepFromScenario(cukeMap[feature], scenarioName);
+      if (myStep) {
+        addScreenshotToStep(screenshotPath, myStep);
+      }
+    });
+};
 
+/**
+ * Overrides the Cucumber Json files with the Json content including the links
+ * @param {string} cukeDirPath path to the folder containing the cucumber jon files
+ * @param {Object} cukeFileMap map linking the feature to the cucumber json file name
+ * @param {Object} cukeMap map linking the feature to the cucumber json content
+ */
+const overrideCucumberFiles = (
+  cukeDirPath: string,
+  cukeFileMap: { [key: string]: string },
+  cukeMap: { [key: string]: IJsonFeature },
+): void => {
+  Object.entries(cukeMap).forEach(([featureName, featureJson]) => {
     // Update the Cucumber logs
     fs.writeFileSync(
-      path.join(jsonPath, jsonNames[feature]),
-      JSON.stringify(cukeMap[feature], null, 2),
+      path.join(cukeDirPath, cukeFileMap[featureName]),
+      // The IJsonFeature object is in one array of one element
+      JSON.stringify([featureJson], null, 2),
     );
-  }
-});
+  });
+};
+
+// Run
+
+readCucumberFiles(cucumberDirPath, cucumberFileMap, cucumberMap);
+addVideoLinks(videosPath, cucumberMap);
+addScreenshotLinks(screenshotsPath, cucumberMap);
+overrideCucumberFiles(cucumberDirPath, cucumberFileMap, cucumberMap);
