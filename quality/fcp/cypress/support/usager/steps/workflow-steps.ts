c22fea1f14caec3d78a603abe4968fb5e329a266
Delalande Olivier <odelalande@kaliop.com>
2021-07-16 14:28:35 +0000

    [FC-590] ETQ QA, je veux couvrir les scénarios désactivation usager depuis FC exploitation

--- quality/fcp/cypress/support/usager/steps/workflow-steps.ts
+++ quality/fcp/cypress/support/usager/steps/workflow-steps.ts
@@ -5,11 +5,13 @@ import IdentityProviderPage from '../pages/identity-provider-page';
 import IdentityProviderSelectionPage from '../pages/identity-provider-selection-page';
 import InfoConsentPage from '../pages/info-consent-page';
 import ServiceProviderPage from '../pages/service-provider-page';
+import TechnicalErrorPage from '../pages/technical-error-page';
 
 const connectionWorkflow = (
   serviceProvider: ServiceProvider,
   identityProvider: IdentityProvider,
   user: User,
+  expectSuccess: boolean,
 ): void => {
   const serviceProviderPage = new ServiceProviderPage(serviceProvider);
   serviceProviderPage.visit();
@@ -25,14 +27,36 @@ const connectionWorkflow = (
   );
   identityProviderPage.login(credentials);
 
-  const infoConsent = new InfoConsentPage();
-  infoConsent.consentButton.click();
-  serviceProviderPage.checkIsUserConnected();
+  if (expectSuccess) {
+    const infoConsent = new InfoConsentPage();
+    infoConsent.consentButton.click();
+    serviceProviderPage.checkIsUserConnected();
+  } else {
+    const technicalErrorPage = new TechnicalErrorPage();
+    technicalErrorPage.checkIsVisible();
+  }
 };
 
 When("l'usager peut se connecter à FranceConnect", function () {
   expect(this.serviceProvider).to.exist;
   expect(this.identityProvider).to.exist;
   expect(this.user).to.exist;
-  connectionWorkflow(this.serviceProvider, this.identityProvider, this.user);
+  connectionWorkflow(
+    this.serviceProvider,
+    this.identityProvider,
+    this.user,
+    true,
+  );
+});
+
+When("l'usager ne peut pas se connecter à FranceConnect", function () {
+  expect(this.serviceProvider).to.exist;
+  expect(this.identityProvider).to.exist;
+  expect(this.user).to.exist;
+  connectionWorkflow(
+    this.serviceProvider,
+    this.identityProvider,
+    this.user,
+    false,
+  );
 });
