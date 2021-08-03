7b7d149d7019574548043041793fffdd376077ed
Nicolas Legeay <nlegeay@kaliopmail.com>
2021-08-03 12:56:46 +0200

    [FC-548] Implement ConnectionWorkflow

--- quality/fcp/cypress/support/usager/steps/workflow-steps.ts
+++ quality/fcp/cypress/support/usager/steps/workflow-steps.ts
@@ -15,67 +15,107 @@ import InfoConsentPage from '../pages/info-consent-page';
 import ServiceProviderPage from '../pages/service-provider-page';
 import TechnicalErrorPage from '../pages/technical-error-page';
 
-const connectionWorkflow = (
-  { allAppsUrl }: Environment,
-  serviceProvider: ServiceProvider,
-  identityProvider: IdentityProvider,
-  user: User,
-  scopeContext: ScopeContext | undefined,
-  expectSuccess: boolean,
-): void => {
-  navigateTo({ appId: serviceProvider.name, baseUrl: allAppsUrl });
-  const serviceProviderPage = new ServiceProviderPage(serviceProvider);
-  if (scopeContext) {
-    serviceProviderPage.setMockRequestedScope(scopeContext);
+class ConnectionWorkflow {
+  allAppsUrl: string;
+  serviceProvider: ServiceProvider;
+  identityProvider: IdentityProvider;
+  serviceProviderPage: ServiceProviderPage;
+
+  constructor({ allAppsUrl }: Environment, serviceProvider: ServiceProvider) {
+    this.allAppsUrl = allAppsUrl;
+    this.serviceProvider = serviceProvider;
   }
-  serviceProviderPage.fcButton.click();
 
-  const identityProviderSelectionPage = new IdentityProviderSelectionPage();
-  identityProviderSelectionPage.checkIsVisible();
-  identityProviderSelectionPage.getIdpButton(identityProvider.idpId).click();
+  init(): ConnectionWorkflow {
+    navigateTo({
+      appId: this.serviceProvider.name,
+      baseUrl: this.allAppsUrl,
+    });
+    this.serviceProviderPage = new ServiceProviderPage(this.serviceProvider);
+    return this;
+  }
 
-  const identityProviderPage = new IdentityProviderPage(identityProvider);
-  const credentials: UserCredentials = user.getCredentials(
-    identityProvider.idpId,
-  );
-  expect(credentials).to.exist;
-  identityProviderPage.login(credentials);
+  withScope(scopeContext: ScopeContext): ConnectionWorkflow {
+    this.serviceProviderPage.setMockRequestedScope(scopeContext);
+    return this;
+  }
 
-  if (expectSuccess) {
-    const infoConsent = new InfoConsentPage();
-    infoConsent.consentButton.click();
-    serviceProviderPage.checkIsUserConnected();
-  } else {
+  start(): ConnectionWorkflow {
+    this.serviceProviderPage.fcButton.click();
+    return this;
+  }
+
+  selectIdentityProvider(
+    identityProvider: IdentityProvider,
+  ): ConnectionWorkflow {
+    this.identityProvider = identityProvider;
+    const identityProviderSelectionPage = new IdentityProviderSelectionPage();
+    identityProviderSelectionPage.checkIsVisible();
+    identityProviderSelectionPage.getIdpButton(identityProvider.idpId).click();
+    return this;
+  }
+
+  login(user: User): ConnectionWorkflow {
+    const identityProviderPage = new IdentityProviderPage(
+      this.identityProvider,
+    );
+    const credentials: UserCredentials = user.getCredentials(
+      this.identityProvider.idpId,
+    );
+    expect(credentials).to.exist;
+    identityProviderPage.login(credentials);
+    return this;
+  }
+
+  checkError(): void {
     const technicalErrorPage = new TechnicalErrorPage();
     technicalErrorPage.checkIsVisible();
   }
-};
+
+  consent(): ConnectionWorkflow {
+    const infoConsent = new InfoConsentPage();
+    infoConsent.consentButton.click();
+    return this;
+  }
+
+  checkConnected(): void {
+    this.serviceProviderPage.checkIsUserConnected();
+  }
+}
 
 When("l'usager peut se connecter à FranceConnect", function () {
   expect(this.env).to.exist;
   expect(this.serviceProvider).to.exist;
   expect(this.identityProvider).to.exist;
   expect(this.user).to.exist;
-  connectionWorkflow(
+  const connectionWorkflow = new ConnectionWorkflow(
     this.env,
     this.serviceProvider,
-    this.identityProvider,
-    this.user,
-    getDefaultScope(this.scopes),
-    true,
   );
+  connectionWorkflow
+    .init()
+    .withScope(getDefaultScope(this.scopes))
+    .start()
+    .selectIdentityProvider(this.identityProvider)
+    .login(this.user)
+    .consent()
+    .checkConnected();
 });
 
 When("l'usager ne peut pas se connecter à FranceConnect", function () {
+  expect(this.env).to.exist;
   expect(this.serviceProvider).to.exist;
   expect(this.identityProvider).to.exist;
   expect(this.user).to.exist;
-  connectionWorkflow(
+  const connectionWorkflow = new ConnectionWorkflow(
     this.env,
     this.serviceProvider,
-    this.identityProvider,
-    this.user,
-    getDefaultScope(this.scopes),
-    false,
   );
+  connectionWorkflow
+    .init()
+    .withScope(getDefaultScope(this.scopes))
+    .start()
+    .selectIdentityProvider(this.identityProvider)
+    .login(this.user)
+    .checkError();
 });
