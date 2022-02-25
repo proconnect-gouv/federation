2b57252f0941d3492a21199b0c13905db1479309
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2022-02-25 18:30:50 +0100

    [FC-901] ETQ usager du user-dashboard je peux mettre à jour mes préférences même si les FIs du legacy sont à un format ancien

--- back/libs/identity-provider-adapter-mongo/src/identity-provider-adapter-mongo.service.spec.ts
+++ back/libs/identity-provider-adapter-mongo/src/identity-provider-adapter-mongo.service.spec.ts
@@ -451,6 +451,9 @@ describe('IdentityProviderAdapterMongoService', () => {
     let validateDtoMock;
     beforeEach(() => {
       validateDtoMock = mocked(validateDto);
+      configMock.get.mockReturnValueOnce({
+        disableIdpValidationOnLegacy: false,
+      });
     });
 
     it('should resolve', async () => {
@@ -477,6 +480,20 @@ describe('IdentityProviderAdapterMongoService', () => {
       expect(repositoryMock.find).toHaveBeenCalledTimes(1);
     });
 
+    it('should get the configuration', async () => {
+      // setup
+      validateDtoMock.mockResolvedValueOnce([]);
+
+      // action
+      await service['findAllIdentityProvider']();
+
+      // expect
+      expect(configMock.get).toHaveBeenCalledTimes(1);
+      expect(configMock.get).toHaveBeenCalledWith(
+        'IdentityProviderAdapterMongo',
+      );
+    });
+
     it('should return result of type list', async () => {
       // setup
       validateDtoMock.mockResolvedValueOnce([]);
@@ -486,6 +503,20 @@ describe('IdentityProviderAdapterMongoService', () => {
 
       // expect
       expect(result).toEqual([legacyIdentityProviderMock]);
+    });
+
+    it('should not call validateDto if the config "disableIdpValidationOnLegacy" is set to "true"', async () => {
+      // setup
+      validateDtoMock.mockResolvedValueOnce(['there is an error']);
+      configMock.get
+        .mockReset()
+        .mockReturnValueOnce({ disableIdpValidationOnLegacy: true });
+
+      // action
+      await service['findAllIdentityProvider']();
+
+      // expect
+      expect(validateDtoMock).toHaveBeenCalledTimes(0);
     });
 
     it('should log a warning if an entry is excluded by the DTO', async () => {
