033eeae268b8124da6eb46546d911cad60563c80
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2022-03-14 17:05:46 +0100

    [FC-873] ETQ DEv, je fais la montée de version des dépendences - Fevrier 2022

--- quality/fcp/cypress/plugins/index.ts
+++ quality/fcp/cypress/plugins/index.ts
@@ -62,17 +62,8 @@ module.exports = (on, config) => {
   if (baseUrls[testEnv]) {
     config.baseUrl = baseUrls[testEnv];
   }
-  if (testEnv === 'integ01') {
-    /**
-     * On integ01 environment, the FC core, FS and FI are using different domains
-     * @link: https://docs.cypress.io/guides/guides/web-security#Same-superdomain-per-test
-     * In order to run the tests,
-     * - we need to disable the chrome web security to allow redirections to different domains
-     * @link: https://docs.cypress.io/guides/guides/web-security#Disabling-Web-Security
-     * - we need to use Cookies with samesite=none (intercept in beforeEach hook)
-     */
-    config.chromeWebSecurity = false;
-  }
+
+  config.chromeWebSecurity = false;
 
   return config;
 };
