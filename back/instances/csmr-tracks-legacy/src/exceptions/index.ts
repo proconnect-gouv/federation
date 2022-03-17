f4ae6531d608561b6b1ad10224427096546b3054
Arnaud Palin Sainte Agathe <arnaud.palin-sainte-agathe@prestataire.modernisation.gouv.fr>
2022-03-17 02:39:57 +0100

    [FC-860] fix: ⚡️ move Exception error to both csmr

--- back/instances/csmr-tracks-legacy/src/exceptions/index.ts
+++ back/instances/csmr-tracks-legacy/src/exceptions/index.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore file */
 
 // Declarative code
-export * from './csmr-tracks-transform-tracks-failed.exception';
 export * from './csmr-tracks-unknown-action.exception';
 export * from './csmr-tracks-unknown-sp.exception';
