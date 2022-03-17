476f59563dd54b4de9fde7e6991f8022a69abaa4
Arnaud Palin Sainte Agathe <arnaud.palin-sainte-agathe@prestataire.modernisation.gouv.fr>
2022-03-17 02:41:44 +0100

    [FC-860] feat: ✅ 🐛 🔥 🎨 ⚡️ update Csmr corehigh with new interface and mock format

--- back/instances/csmr-tracks-high/src/services/csmr-tracks-data-high.service.ts
+++ back/instances/csmr-tracks-high/src/services/csmr-tracks-data-high.service.ts
@@ -1,35 +1,23 @@
 import { Search } from '@elastic/elasticsearch/api/requestParams';
-import { pick } from 'lodash';
 
 import { Injectable } from '@nestjs/common';
 
-import { IAppTracksDataService } from '@fc/csmr-tracks';
+import {
+  CsmrTracksTransformTracksFailedException,
+  IAppTracksDataService,
+} from '@fc/csmr-tracks';
 import { formatMultiMatchGroup } from '@fc/elasticsearch';
-import { LoggerService } from '@fc/logger';
+import { LoggerLevelNames, LoggerService } from '@fc/logger';
 import { ICsmrTracksOutputTrack } from '@fc/tracks';
 
-import { ICsmrTracksHighTracks, ICsmrTracksInputHigh } from '../interfaces';
-
-/**
- * Array of available track's object attributes
- * used to validate the tracks received from Elasticsearch.
- * @see ICsmrTracksOutputTrack
- */
-const TRACK_PROPERTIES = [
-  'event',
-  'date',
-  'spName',
-  'spAcr',
-  'country',
-  'city',
-];
+import { ICsmrTracksHighTrack, ICsmrTracksInputHigh } from '../interfaces';
 
 export const PLATFORM = 'FranceConnect+';
 
 const SIX_MONTHS_AGO = 'now-6M/d';
 const NOW = 'now';
 
-const EVENTS_TO_INCLUDE: Partial<ICsmrTracksHighTracks>[] = [
+const EVENTS_TO_INCLUDE: Partial<ICsmrTracksHighTrack>[] = [
   {
     event: 'FC_VERIFIED',
   },
@@ -43,6 +31,12 @@ const EVENTS_TO_INCLUDE: Partial<ICsmrTracksHighTracks>[] = [
     event: 'DP_REQUESTED_FC_CHECKTOKEN',
   },
 ];
+
+type OutputTrack = Omit<ICsmrTracksOutputTrack, 'platform' | 'trackId'>;
+interface GeoIp {
+  country: string;
+  city: string;
+}
 
 @Injectable()
 export class CsmrTracksHighDataService implements IAppTracksDataService {
@@ -86,22 +80,78 @@ export class CsmrTracksHighDataService implements IAppTracksDataService {
     return query;
   }
 
+  private getClaimsGroups({ claims }: ICsmrTracksHighTrack): string[] | null {
+    /**
+     * @todo #820
+     * add scope and label management here
+     *
+     * Arnaud PSA: 07/02/2022
+     */
+    return claims ? claims.split(' ') : null;
+  }
+
+  private async getGeoFromIp({ ip: _ }: ICsmrTracksHighTrack): Promise<GeoIp> {
+    /**
+     * @todo add GeoIp management here
+     *
+     * Arnaud PSA: 07/02/2022
+     */
+    return {
+      country: 'FR',
+      city: 'Paris',
+    };
+  }
+
+  async transformTrack(source: ICsmrTracksHighTrack) {
+    const { event, time, spName, spAcr, idpName } = source;
+
+    const claims = this.getClaimsGroups(source);
+    const { country, city } = await this.getGeoFromIp(source);
+
+    const output: OutputTrack = {
+      event,
+      time,
+      spName,
+      spAcr,
+      country,
+      city,
+      claims,
+      idpName,
+    };
+
+    this.logger.trace({ source, output });
+    return output;
+  }
+
   /**
    * Get formated tracks reduced to their strict elements.
    * Elasticsearch add extra attributes to the stored data
-   * that are not requiered.
+   * that are not required.
    */
   async formattedTracks(
     rawTracks: ICsmrTracksInputHigh[],
   ): Promise<ICsmrTracksOutputTrack[]> {
     this.logger.debug('formattedTracks from core-high');
+
     const filteredProperties = rawTracks.map(
-      ({ _id: trackId, _source: source }) => {
-        const properties = pick(source, TRACK_PROPERTIES);
-        const platform = PLATFORM;
-        return { ...properties, trackId, platform, claims: null };
+      async ({ _id: trackId, _source: source }) => {
+        const data = await this.transformTrack(source);
+        return {
+          ...data,
+          trackId,
+          platform: PLATFORM,
+        };
       },
     );
-    return filteredProperties as ICsmrTracksOutputTrack[];
+    try {
+      const tracks = await Promise.all(filteredProperties);
+
+      this.logger.trace({ tracks });
+
+      return tracks;
+    } catch (error) {
+      this.logger.trace({ error }, LoggerLevelNames.WARN);
+      throw new CsmrTracksTransformTracksFailedException(error);
+    }
   }
 }
