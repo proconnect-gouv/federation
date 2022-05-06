195e0768fb2598d3b73988146823bcfd336aec5d
Arnaud Palin Sainte Agathe <arnaud.palin-sainte-agathe@prestataire.modernisation.gouv.fr>
2022-05-06 17:26:49 +0200

    [FC-878] ETQ User, je souhaite obtenir les scopes par famille sur la page des traces du UserDashBoard

--- back/instances/csmr-tracks-high/src/config/index.ts
+++ back/instances/csmr-tracks-high/src/config/index.ts
@@ -8,11 +8,13 @@ import GeoipMaxmind from './geoip-maxmind';
 import Logger from './logger';
 import IdpMappings from './mappings';
 import Mongoose from './mongoose';
+import Scopes from './scopes';
 import TracksBroker from './tracks-broker';
 
 export default {
   Logger,
   Mongoose,
+  Scopes,
   TracksBroker,
   GeoipMaxmind,
   Elasticsearch,
