4b0ec5519b345122fed84b31a4dd25351136a963
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2023-06-08 12:08:05 +0200

    [FC-1099] ⚰️  Remove mongo-partenaires container

--- docker/volumes/mongo-partenaires/scripts/db-states/_default/index.js
+++
@@ -1,9 +0,0 @@
-print('Initializing SPs...');
-load('/opt/scripts/db-states/_default/sp.js');
-
-/* ------------------------------------------------------------------------------- */
-
-print('Initializing Partners...');
-load('/opt/scripts/db-states/_default/partners.js');
-
-/* ------------------------------------------------------------------------------- */
