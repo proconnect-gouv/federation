a3c334e14f37c02febf1c609426586550a80bcef
gdarquie <gaetan.darquie@beta.gouv.fr>
2023-08-28 11:39:41 +0200

    FA-1366: add redirection logic but without dynamic id and csrf

--- front/instances/agent-connect/src/ui/components/search-results/search-results.component.tsx
+++ front/instances/agent-connect/src/ui/components/search-results/search-results.component.tsx
@@ -1,14 +1,20 @@
 import classnames from 'classnames';
 import React from 'react';
 
+import { useAddToUserHistory } from '@fc/agent-connect-history';
 import { useSearchResults } from '@fc/agent-connect-search';
+import { RedirectToIdpFormComponent } from '@fc/oidc-client';
 
 import styles from './search-results.module.scss';
 import { SearchResultsListComponent } from './search-results-list.component';
 
-
 export const SearchResultsComponent = React.memo(() => {
   const { searchResults, showNoResults, showResults } = useSearchResults();
+  const uid = 'uuid';
+  const csrfToken = 'csrfToken';
+
+  const addToUserHistory = useAddToUserHistory(uid);
+
   return (
     <React.Fragment>
       {showResults && <SearchResultsListComponent results={searchResults} />}
@@ -17,17 +23,25 @@ export const SearchResultsComponent = React.memo(() => {
           <div className="fr-mx-2w fr-text--lg" id="identity-provider-result">
             <p>Aucun résultat. Vous pouvez vous connecter en utilisant MonComptePro.</p>
           </div>
-          <div>
-            <button aria-label="Se connecter à MonComptePro" className={classnames(styles.moncompteprobutton)}/>
+          <RedirectToIdpFormComponent csrf={csrfToken} id={`fca-search-idp-${uid}`} uid={uid}>
+            <button
+              aria-label="Se connecter à MonComptePro"
+              className={classnames(styles.moncompteprobutton)}
+              id={`idp-${uid}-button`}
+              type="submit"
+              onClick={addToUserHistory}
+            />
             <p>
-              <a href="https://moncomptepro.beta.gouv.fr/" rel="noopener noreferrer" target="_blank"
+              <a
+                href="https://moncomptepro.beta.gouv.fr/"
+                rel="noopener noreferrer"
+                target="_blank"
                 title="Qu’est-ce que MonComptePro ? - nouvelle fenêtre">
                 Qu’est-ce que MonComptePro ?
               </a>
             </p>
-          </div>
+          </RedirectToIdpFormComponent>
         </div>
-
       )}
     </React.Fragment>
   );
