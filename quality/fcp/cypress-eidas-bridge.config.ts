bbbda1f5d0015b29534ea9fabbf4ae5eb39be7b0
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2023-10-06 10:35:47 +0200

    [FC-1325] eIDAS : Cohérence des traces entre sessions

--- quality/fcp/cypress-eidas-bridge.config.ts
+++ quality/fcp/cypress-eidas-bridge.config.ts
@@ -4,6 +4,16 @@ import { defineConfig } from 'cypress';
 
 import pluginConfig from './cypress/plugins';
 
+const FLOW_CONSISTENCY_GLOBAL = [
+  'level',
+  'time',
+  'pid',
+  'hostname',
+  'event',
+  'category',
+  'step',
+];
+
 export default defineConfig({
   e2e: {
     baseUrl: 'https://docker.dev-franceconnect.fr',
@@ -25,7 +35,162 @@ export default defineConfig({
     // Test environment access
 
     // Other Configuration
-    LOG_FILE_PATH: "../../docker/volumes/log/core-fcp-high.log",
-    EIDAS_LOG_FILE_PATH: "../../docker/volumes/log/eidas-bridge.log",
+    LOG_FILE_PATH: '../../docker/volumes/log/core-fcp-high.log',
+    EIDAS_LOG_FILE_PATH: '../../docker/volumes/log/eidas-bridge.log',
+    FLOW_CONSISTENCY_FR_EU: [
+      {
+        event: 'INCOMING_EIDAS_REQUEST',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'logId',
+        ],
+      },
+      {
+        event: 'REDIRECT_TO_FC_AUTHORIZE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'RECEIVED_FC_AUTH_CODE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'REDIRECTING_TO_EIDAS_FR_NODE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'eidasLevelReceived',
+          'idpSub',
+          'spSub',
+          'logId',
+        ],
+      },
+    ],
+    FLOW_CONSISTENCY_EU_FR: [
+      {
+        event: 'INCOMING_FC_REQUEST',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'source',
+          'sessionId',
+          'logId',
+        ],
+      },
+      {
+        event: 'DISPLAYING_CITIZEN_COUNTRY_CHOICE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'SELECTED_CITIZEN_COUNTRY',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'REDIRECTING_TO_FR_NODE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'INCOMING_EIDAS_RESPONSE',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'logId',
+        ],
+      },
+      {
+        event: 'REDIRECT_TO_FC',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'eidasLevelReceived',
+          'idpSub',
+          'spSub',
+          'logId',
+        ],
+      },
+      {
+        event: 'RECEIVED_CALL_ON_TOKEN',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'eidasLevelReceived',
+          'idpSub',
+          'spSub',
+          'logId',
+        ],
+      },
+      {
+        event: 'RECEIVED_CALL_ON_USERINFO',
+        keys: [
+          ...FLOW_CONSISTENCY_GLOBAL,
+          'countryCodeSrc',
+          'countryCodeDst',
+          'source',
+          'sessionId',
+          'eidasLevelRequested',
+          'eidasLevelReceived',
+          'idpSub',
+          'spSub',
+          'logId',
+        ],
+      },
+    ],
   },
 });
