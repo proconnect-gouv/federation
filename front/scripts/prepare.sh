9454d0d9ef17f118f118cb2a1c9c4f104011ffbf
Matthieu Lassalvy <matthieu.lassalvy@prestataire.modernisation.gouv.fr>
2024-04-02 15:56:40 +0200

    [#1612] 🦆 Split des css dans une lib indépendante @fc/styles

--- front/scripts/prepare.sh
+++ front/scripts/prepare.sh
@@ -25,3 +25,31 @@ copy_dsfr_files() {
   create_folder_path "$DEST_FOLDER/dsfr/utility"
   copy_file "$DSFR_FOLDER/utility/utility.min.css" "$DEST_FOLDER/dsfr/utility/utility.min.css"
 }
+
+build_and_copy_fc_stylesheet_to_instances_public_folder() {
+  # @NOTE Sass Cli do not have any node_modules resolution
+  # if we would like to manage custom importer like `~`
+  # We should install a third-party plugin or using a build tool like rollup
+  MODULES_PATH="$(pwd)/node_modules"
+
+  SRC_FOLDER="./libs/styles"
+  DEST_FOLDER="./instances/$1/public"
+
+  check_source_and_files "$MODULES_PATH"
+  check_source_and_files "$SRC_FOLDER"
+  check_source_and_files "$DEST_FOLDER"
+
+  SRC_FILE="$SRC_FOLDER/src/index.scss"
+  OUTPUT_FILE="$DEST_FOLDER/fc/fc.min.css"
+
+  print_info "Building $OUTPUT_FILE..."
+  yarn sass \
+    --style=compressed \
+    --load-path="$MODULES_PATH" "$SRC_FILE" "$OUTPUT_FILE"
+
+  if [ $? -ne 0 ]; then
+    print_error "$OUTPUT_FILE build has failed successfully !"
+    exit 1
+  fi
+  print_success "$OUTPUT_FILE created successfully !"
+}
--- front/scripts/yarn-start.sh
+++ front/scripts/yarn-start.sh
@@ -17,6 +17,7 @@ source "./scripts/prepare.sh"
 # ---------------------------------------
 check_app_argument_exists "$1"
 copy_dsfr_files "$1"
+build_and_copy_fc_stylesheet_to_instances_public_folder "$1"
 
 INSTANCE_FOLDER="./instances/$1"
 cd "$INSTANCE_FOLDER"
