76f2db92a6e8df6855489398dd8b92c0f82299ed
Hugues Charleux <h@chrx.io>
2024-04-29 16:14:38 +0200

    [POC-1536] Log-hub + log prod

--- docker/bash/commands/node.sh
+++ docker/bash/commands/node.sh
@@ -8,9 +8,35 @@ _log() {
 
 _start() {
   local apps="${@:-no-container}"
+  _clean_fc_dist "${apps}"
+
   for app in ${apps}; do
     task "   * Starting app \e[3m${app}\e[0m" \
       "_do_start" "${app}"
+  done
+
+  # Reload RP in case the app took to long and was consired down by Nginx
+  task "   * Reload RP" "_reload_rp"
+}
+
+_start_prod() {
+  local apps="${@:-no-container}"
+  _clean_fc_dist "${apps}"
+
+  for app in ${apps}; do
+    task "   * Starting app \e[3m${app}\e[0m" \
+      "_do_start_prod" "${app}"
+  done
+
+  # Reload RP in case the app took to long and was consired down by Nginx
+  task "   * Reload RP" "_reload_rp"
+}
+
+_start_dev() {
+  local apps="${@:-no-container}"
+  for app in ${apps}; do
+    task "   * Starting app \e[3m${app}\e[0m" \
+      "_do_start_dev" "${app}"
   done
 
   # Reload RP in case the app took to long and was consired down by Nginx
@@ -28,6 +54,29 @@ _start_ci() {
   task "   * Reload RP" "_reload_rp"
 }
 
+_detect_instances() {
+  local apps="${@:-no-container}"
+  local instances=$(
+    for app in ${apps}; do
+      $DOCKER_COMPOSE exec ${NO_TTY} "${app}" env | grep -oP '(?<=NESTJS_INSTANCE=)[^=]+'
+    done
+  )
+
+  echo "${instances}" | sort | uniq | grep -oE "[a-zA-Z0-9-]+"
+}
+
+_clean_fc_dist() {
+  local apps="${@:-no-container}"
+  local instances=$(_detect_instances "${apps}")
+
+  cd ${VOLUMES_DIR}
+
+  for instance in ${instances}; do
+    echo "    * Purging build dir for ${instance}"
+    rm -rf "src/fc/back/dist/instances/${instance}"
+  done
+}
+
 _do_start() {
   local app=$1
 
@@ -40,6 +89,20 @@ _do_start_ci() {
 
   cd ${WORKING_DIR}
   $DOCKER_COMPOSE exec ${NO_TTY} "${app}" "/opt/scripts/start-ci.sh"
+}
+
+_do_start_dev() {
+  local app=$1
+
+  cd ${WORKING_DIR}
+  $DOCKER_COMPOSE exec ${NO_TTY} "${app}" "/opt/scripts/start-dev.sh"
+}
+
+_do_start_prod() {
+  local app=$1
+
+  cd ${WORKING_DIR}
+  $DOCKER_COMPOSE exec ${NO_TTY} "${app}" "/opt/scripts/start-prod.sh"
 }
 
 _start_all() {
