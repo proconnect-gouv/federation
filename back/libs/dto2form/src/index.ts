3db10994882b70833b76cd2fee28c700c704a7fa
Stéphane Herman <stephane.herman@modernisation.gouv.fr>
2024-06-07 13:25:16 +0200

    [FC-1567] IssueMise en place de la librairie dto2form

---
+++ back/libs/dto2form/src/index.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+
+// declarative file
+export * from './classes';
+export * from './decorators';
+export * from './descriptors';
+export * from './dto2form.module';
+export * from './interfaces';
+export * from './pipes';
