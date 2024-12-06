d2ba60fb3da0b56d1c9a912bd931430c09478219
Gaétan Darquié <gaetan.darquie@beta.gouv.fr>
2024-12-06 10:46:55 +0100

    [PC-2006] Importer dynamiquement le module Core via Nest

--- back/apps/core-fca/src/interfaces/index.ts
+++ back/apps/core-fca/src/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './core-fca-authorization-parameters.interface';
 export * from './core-fca-fqdn-service.interface';
 export * from './core-fca-service.interface';
 export * from './core-fca-tracking-log';
+export * from './core-tracking-context.interface';
