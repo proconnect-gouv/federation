2a5bb38a05fb83739b8b6b21e2217c48bc8aff3e
Gaétan Darquié <gaetan.darquie@beta.gouv.fr>
2025-02-06 15:37:55 +0100

    [PC-2107] Ajout du champ email dans le tracking

--- back/apps/core-fca/src/interfaces/core-tracking-context.interface.ts
+++ back/apps/core-fca/src/interfaces/core-tracking-context.interface.ts
@@ -2,4 +2,6 @@ import { ICoreTrackingContext } from '@fc/core';
 
 export interface CoreFcaTrackingContextInterface extends ICoreTrackingContext {
   readonly fqdn?: string;
+  readonly email?: string;
+  readonly idpSub?: string;
 }
