f6a60fad00197ce8a5c608a4c4154901ddb7e2bb
rebeccadumazert <rebecca.dumazert@beta.gouv.fr>
2025-02-18 15:37:40 +0100

    [PC-2139]: 🛠️  Fix broken Matomo stats

--- back/apps/core-fca/src/public/js/matomo.js
+++ back/apps/core-fca/src/public/js/matomo.js
@@ -1,10 +1,28 @@
-var _mtm = (window._mtm = window._mtm || []);
-_mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
+var _paq = (window._paq = window._paq || []);
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push([
+  'setExcludedQueryParams',
+  [
+    'session_state',
+    'state',
+    'code',
+    'id_token_hint',
+    'nonce',
+    'login_hint',
+    'simulationId',
+    '_csrf',
+  ],
+]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
 (function () {
+  var u = 'https://stats.data.gouv.fr/';
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', '295']);
   var d = document,
     g = d.createElement('script'),
     s = d.getElementsByTagName('script')[0];
   g.async = true;
-  g.src = 'https://stats.data.gouv.fr/js/container_663J4cs0.js';
+  g.src = u + 'matomo.js';
   s.parentNode.insertBefore(g, s);
 })();
