6cf738a80c2aaa2345f649147871c40c0cfcd163
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-05-15 16:13:48 +0200

    chore(matomo): add tag manager

--- back/apps/core-fca/src/public/js/matomo.js
+++ back/apps/core-fca/src/public/js/matomo.js
@@ -1,28 +1,10 @@
-var _paq = (window._paq = window._paq || []);
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push([
-  'setExcludedQueryParams',
-  [
-    'session_state',
-    'state',
-    'code',
-    'id_token_hint',
-    'nonce',
-    'login_hint',
-    'simulationId',
-    '_csrf',
-  ],
-]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+var _mtm = (window._mtm = window._mtm || []);
+_mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
 (function () {
-  var u = 'https://stats.data.gouv.fr/';
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', '295']);
   var d = document,
     g = d.createElement('script'),
     s = d.getElementsByTagName('script')[0];
   g.async = true;
-  g.src = u + 'matomo.js';
+  g.src = 'https://stats.data.gouv.fr/js/container_663J4cs0.js';
   s.parentNode.insertBefore(g, s);
 })();
