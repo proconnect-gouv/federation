305801baebee7c5c0367d43f188ac89cf2a84a61
Laurent Bossavit <laurent.git@bossavit.com>
2025-06-22 20:40:12 +0200

    🔥 Remove a tiny bit of dead code that results in annoying CI failures

--- back/libs/common/src/helpers/wait.ts
+++
@@ -1,5 +0,0 @@
-// Actually return a promise
-// eslint-disable-next-line require-await
-export async function wait(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
