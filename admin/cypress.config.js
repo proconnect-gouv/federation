d577df06564f4acd671ba29289d585729a66d2fd
Benoit Serrano <benoit.serrano10@gmail.com>
2025-08-07 17:08:17 +0300

    🎨 move fc-exploitation folder upward

--- admin/cypress.config.js
+++ admin/cypress.config.js
@@ -3,34 +3,34 @@ const { defineConfig } = require('cypress');
 const pluginConfig = require('./cypress/plugins');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://exploitation-fca-low.docker.dev-franceconnect.fr',
+    excludeSpecPattern: 'cypress/integration/**/*.utils.js',
+    experimentalRunAllSpecs: true,
+    retries: 2,
     setupNodeEvents(on, config) {
       return pluginConfig(on, config);
     },
     specPattern: 'cypress/integration/**/*.js',
-    excludeSpecPattern: 'cypress/integration/**/*.utils.js',
     supportFile: 'cypress/support/index.js',
-    retries: 2,
     video: false,
-    experimentalRunAllSpecs: true,
   },
   env: {
-    APP_NAME: 'exploitation-fca-low',
-    APP_HOME_ROLE_ADMIN:
-      'https://exploitation-fca-low.docker.dev-franceconnect.fr/account',
-    APP_HOME_ROLE_OPERATOR:
-      'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
-    APP_HOME_ROLE_SECURITY:
-      'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
     APP_FORBIDDEN_PAGE:
-      'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
+    'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
+    APP_HOME_ROLE_ADMIN:
+    'https://exploitation-fca-low.docker.dev-franceconnect.fr/account',
+    APP_HOME_ROLE_OPERATOR:
+    'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
+    APP_HOME_ROLE_SECURITY:
+    'https://exploitation-fca-low.docker.dev-franceconnect.fr/service-provider',
+    APP_NAME: 'exploitation-fca-low',
     FEDERATION_DIR: `${process.env.PC_ROOT}/federation`,
     LOG_FILE_PATH: `${process.env.PC_ROOT}/federation/docker/volumes/log/fcexploitation.log`,
     TOTP_WINDOW: 'loose',
   },
-  chromeWebSecurity: false,
-  viewportHeight: 1800,
-  viewportWidth: 1400,
   pageLoadTimeout: 30000,
+  viewportHeight: 1800,
+  viewportWidth: 1400
 });
