76d3d478cfe9ff70c40d0e938fbf3bf197b9f220
Benoit Serrano <benoit.serrano10@gmail.com>
2025-08-08 13:36:49 +0300

    🔥 remove query-string package

--- admin/src/meta/locals.interceptor.ts
+++ admin/src/meta/locals.interceptor.ts
@@ -7,7 +7,6 @@ import {
 import { Observable } from 'rxjs';
 import { ConfigService } from 'nestjs-config';
 import * as moment from 'moment-timezone';
-import * as queryString from 'query-string';
 import { errorCodeTranslations } from './error-code-translations';
 import { UserRole } from '../user/roles.enum';
 import { VALID_INPUT_STRING_REGEX } from '../utils/validators/is-valid-input-string';
@@ -51,7 +50,6 @@ export class LocalsInterceptor implements NestInterceptor {
     res.locals.CURRENT_USER = req.user;
 
     res.locals.REQ_QUERY = req.query;
-    res.locals.queryString = queryString;
 
     res.locals.moment = moment;
     res.locals.title = appConfig.appName;
