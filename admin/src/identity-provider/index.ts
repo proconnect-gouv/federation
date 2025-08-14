09e07ef718039c89a41825e31245e1fbba7d3b95
Benoit Serrano <benoit.serrano10@gmail.com>
2025-08-14 17:46:32 +0200

    🔥 remove useless fixtures and enum

--- admin/src/identity-provider/index.ts
+++ admin/src/identity-provider/index.ts
@@ -1,7 +1,6 @@
 /* istanbul ignore file */
 
 // Declarative code
-export * from './fixture';
 export * from './identity-provider.module';
 export * from './identity-provider.service';
 export * from './interface';
