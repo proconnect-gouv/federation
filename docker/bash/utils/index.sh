5993739560699ee41c9430958fa0ac1fcf1115af
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2025-08-20 18:20:50 +0200

    join_byebye

--- docker/bash/utils/index.sh
+++ docker/bash/utils/index.sh
@@ -2,6 +2,5 @@
 
 source "${DOCKER_BASH_DIR}/utils/commands.sh"
 source "${DOCKER_BASH_DIR}/utils/format.sh"
-source "${DOCKER_BASH_DIR}/utils/join-by.sh"
 source "${DOCKER_BASH_DIR}/utils/task.sh"
 source "${DOCKER_BASH_DIR}/utils/wait.sh"
