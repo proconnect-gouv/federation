798fd8bc3c7b4054ef20ec99e07e658ac5df7b24
gdarquie <gaetan.darquie@gmail.com>
2025-10-03 18:23:34 +0200

    ♻ Get idp from email after fqdn/idp refacto
    Recently, a refactor moved the FQDNs from the FqdnToProvider collection
    into the providers collection: a provider can now have multiple FQDNs, stored as strings.
    
    Following this refactor, we no longer need to query FQDNs and IDPs in two separate queries.
    This PR centralizes the retrieval of IDPs and FQDNs
    into a single request and adapts the code accordingly.
    
    I also used this PR as an opportunity to make minor variable name changes
    for consistency with the rest of the codebase
    (e.g., replacing `provider` with `identityProvider`, or `coreFca` with `coreFcaService`).
    
    I extracted into a dedicated function the logic
    that renames the default IDP in a list,
    if present, and sorts them alphabetically when there is more than one.
    
    While making these changes, I realized we could further improve
    by avoiding duplicate calls for IDPs when displaying the IDP selection view in a future step.

--- back/apps/core-fca-low/src/services/core-fca-fqdn.service.ts
+++ back/apps/core-fca-low/src/services/core-fca-fqdn.service.ts
@@ -1,48 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 import { ConfigService } from '@fc/config';
-import { IdentityProviderAdapterMongoService } from '@fc/identity-provider-adapter-mongo';
 
 import { AppConfig } from '../dto';
-import { FqdnConfigInterface } from '../interfaces';
+import { SpAuthorizedFqdnsDetails } from '../interfaces';
 
 @Injectable()
 export class CoreFcaFqdnService {
-  constructor(
-    private readonly identityProviderAdapterMongoService: IdentityProviderAdapterMongoService,
-    private readonly config: ConfigService,
-  ) {}
-  async getFqdnConfigFromEmail(email: string): Promise<FqdnConfigInterface> {
-    const { defaultIdpId } = this.config.get<AppConfig>('App');
-    const fqdn =
-      this.identityProviderAdapterMongoService.getFqdnFromEmail(email);
-    const idpsByFqdn =
-      await this.identityProviderAdapterMongoService.getIdpsByFqdn(fqdn);
+  constructor(private readonly config: ConfigService) {}
 
-    // when there is no idp mapped for this fqdn
-    // we check if there is or not a default idp set in the app
-    // if yes, we return the default idp
-    // if no, we return an empty config and we deduce that the default idp is not accepted
-    if (idpsByFqdn.length === 0) {
-      return {
-        fqdn,
-        identityProviderIds: defaultIdpId ? [defaultIdpId] : [],
-      };
-    }
-
-    const identityProviderIds = idpsByFqdn.map(({ uid }) => uid);
-
-    return {
-      fqdn,
-      identityProviderIds,
-    };
-  }
-
-  getSpAuthorizedFqdnsConfig(spId: string): {
-    spName: string;
-    spContact: string;
-    authorizedFqdns: string[];
-  } | null {
+  getSpAuthorizedFqdnsDetails(spId: string): SpAuthorizedFqdnsDetails | null {
     const { spAuthorizedFqdnsConfigs } = this.config.get<AppConfig>('App');
 
     return (
@@ -50,21 +17,5 @@ export class CoreFcaFqdnService {
         return config.spId === spId;
       }) || null
     );
-  }
-
-  async isAllowedIdpForEmail(idpId: string, email: string): Promise<boolean> {
-    const identityProvider =
-      await this.identityProviderAdapterMongoService.getById(idpId);
-
-    const emailFqdn =
-      this.identityProviderAdapterMongoService.getFqdnFromEmail(email);
-
-    if (identityProvider.fqdns?.some((fqdn) => fqdn === emailFqdn)) {
-      return true;
-    }
-
-    // if no idp explicitly handles the domain, the only idp allowed is the default one
-    const { defaultIdpId } = this.config.get<AppConfig>('App');
-    return defaultIdpId === idpId;
   }
 }
