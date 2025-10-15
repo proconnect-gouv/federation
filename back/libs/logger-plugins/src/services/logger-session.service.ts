4a11fe4cdc2540b0960d0cc2693ede299176dd31
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2025-10-15 17:36:59 +0200

    Add session data to libs/logger output
    Previously, error logs (emitted via libs/logger) did not contain session
    data (e.g. idpName, etc.), which for example made it difficult to
    categorize errors by IdP or SP.
    
    We now include the same session data in error logs (as emitted by
    libs/logger via the LoggerSessionPlugin) as is included in tracked
    "business events".

--- back/libs/logger-plugins/src/services/logger-session.service.ts
+++ back/libs/logger-plugins/src/services/logger-session.service.ts
@@ -15,11 +15,47 @@ export class LoggerSessionService implements LoggerPluginServiceInterface {
     });
 
     const sessionId = sessionService.getId();
-    const { browsingSessionId } = sessionService.get<UserSession>('User') || {};
+    const {
+      amr,
+      browsingSessionId,
+      interactionId,
+      interactionAcr,
+      spId,
+      spEssentialAcr,
+      spName,
+      spIdentity,
+      spLoginHint,
+      idpId,
+      idpAcr,
+      idpName,
+      idpLabel,
+      idpLoginHint,
+      idpIdentity,
+    } = sessionService.get<UserSession>('User') || {};
 
     const context = {
-      sessionId,
+      amr,
       browsingSessionId,
+      idpAcr,
+      idpEmail: idpIdentity?.email,
+      idpEmailFqdn: idpIdentity?.email?.split('@').pop().toLowerCase(),
+      idpId,
+      idpLabel,
+      idpLoginHint,
+      idpLoginHintFqdn: idpLoginHint?.split('@').pop().toLowerCase(),
+      idpName,
+      idpSiret: idpIdentity?.siret,
+      idpSub: idpIdentity?.sub,
+      interactionAcr,
+      interactionId,
+      sessionId,
+      spEssentialAcr,
+      spLoginHint,
+      spLoginHintFqdn: spLoginHint?.split('@').pop().toLowerCase(),
+      spId,
+      spName,
+      spSub: spIdentity?.sub,
+      spSiret: spIdentity?.siret,
     };
 
     return context;
