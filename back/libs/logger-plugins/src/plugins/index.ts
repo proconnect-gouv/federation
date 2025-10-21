ae4f34ce7de43ec1781036831646526002cf6a70
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2025-10-21 14:39:20 +0200

    🔥 Remove useless logger plugin
    **Problem**
    Most of the time, the stacktrace key shows a *Filter.logException* call
    which is not useful for debugging and adds a lot of noise.
    Plus this plugin is also active in production environment which should
    not be the case due to Logger_THRESHOLD set to DEBUG in production environment.
    
    **Proposal**
    Remove this plugin solves the first problem.
    The second one is to set Logger_THRESHOLD to INFO in production environment.
    see: https://github.com/proconnect-gouv/infra-legacy/pull/77

--- back/libs/logger-plugins/src/plugins/index.ts
+++ back/libs/logger-plugins/src/plugins/index.ts
@@ -1,3 +1,2 @@
-export * from './logger-debug.plugin';
 export * from './logger-request.plugin';
 export * from './logger-session.plugin';
--- back/libs/logger-plugins/src/services/index.ts
+++ back/libs/logger-plugins/src/services/index.ts
@@ -1,3 +1,2 @@
-export * from './logger-debug.service';
 export * from './logger-request.service';
 export * from './logger-session.service';
