a1dde94699347ce9246d8af5fe7523b3910bc4d9
gdarquie <gaetan.darquie@gmail.com>
2026-01-05 20:06:28 +0900

    🎨 Simplify admin hook initialization
    Remove unnecessary comments and inline hook initialization code
     with minor simplifications for better readability.

--- docker/bash/commands/up.sh
+++ docker/bash/commands/up.sh
@@ -4,17 +4,6 @@ export  NODE_VERSION
 
 DOCKER_COMPOSE="docker compose"
 
-function _hook_admin() {
-  local app=$1
-  echo "  Fixtures for ${app} app..."
-  cd ${WORKING_DIR}
-  ${DOCKER_COMPOSE} exec ${NO_TTY} "${app}" yarn typeorm schema:drop
-  ${DOCKER_COMPOSE} exec ${NO_TTY} "${app}" yarn migrations:run
-  ${DOCKER_COMPOSE} exec ${NO_TTY} "${app}" yarn fixtures:load
-
-  (cd ${FEDERATION_DIR}/admin/cypress/support/ && ./db.sh ${app} create)
-}
-
 _up() {
   # get asked services
   local services=${@}
@@ -28,14 +17,6 @@ _up() {
   echo " * Running hooks for started containers"
   local started_services=$($DOCKER_COMPOSE ps --format '{{.Service}}')
   for app in ${started_services}; do
-    # Container initialization hooks
-    #
-    # This runs arbitrary code if a container is started
-    # matching on the container name
-    #
-    # Hooks are called in the `docker-stack up <stack>` command,
-    # after all other automatic procedures.
-    # Nodejs dependencies are already installed at this stage
     case $app in
     *"lemon-ldap"*)
       echo "Restore LemonLDAP configuration"
@@ -47,7 +28,13 @@ _up() {
       _reset_mongodb "$app"
       ;;
     *"pg-admin")
-       _hook_admin "admin"
+      echo "  Fixtures for admin app..."
+      cd ${WORKING_DIR}
+      ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn typeorm schema:drop
+      ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn migrations:run
+      ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn fixtures:load
+
+      (cd ${FEDERATION_DIR}/admin/cypress/support/ && ./db.sh "admin" create)
       ;;
     *)
       ;;
