21c8bd11cd1e43f14df15e5c7c9746631215710f
BenoitSerrano <benoit.serrano10@gmail.com>
2026-03-06 12:38:19 +0100

    🗑️ delete useless backup script in admin

--- docker/bash/commands/up.sh
+++ docker/bash/commands/up.sh
@@ -33,8 +33,6 @@ _up() {
       ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn typeorm schema:drop
       ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn migrations:run
       ${DOCKER_COMPOSE} exec ${NO_TTY} "admin" yarn fixtures:load
-
-      (cd ${FEDERATION_DIR}/admin/cypress/support/ && docker exec pc-pg-"admin"-1 bash -c "$(cat create-db-backup.sh)")
       ;;
     *)
       ;;
