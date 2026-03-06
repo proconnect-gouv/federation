35643c4a8d79c06222b334078a722ac6c7629c65
gdarquie <gaetan.darquie@beta.gouv.fr>
2026-03-06 12:51:19 +0100

    ⚗️ Add a sleep before creating backup db for psql

--- admin/cypress/support/create-db-backup.sh
+++ admin/cypress/support/create-db-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## backup is used by db.sh
 psql postgres -U pg-user -c 'REVOKE CONNECT ON DATABASE "pg-backup" FROM public'
 psql postgres -U pg-user -c "SELECT \"pid\", pg_terminate_backend(pid) FROM \"pg_stat_activity\" WHERE datname='pg-backup' AND pid <> pg_backend_pid();"
 psql postgres -U pg-user -c 'DROP DATABASE  IF EXISTS "pg-backup"'
