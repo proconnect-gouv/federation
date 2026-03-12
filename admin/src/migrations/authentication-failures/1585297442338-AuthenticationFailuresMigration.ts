a51bb280e5b930bb410531217ac8a951e4eecc21
Douglas DUTEIL <douglas.duteil@beta.gouv.fr>
2026-03-12 12:44:37 +0100

    🧹 (admin) use prettier default config
    **Problem**
    
    Custom prettier config is discouraged.
    
    **Proposal**
    
    Use the default config and format all possible files.
    *Note* : This is a proposal, seems to still be influenced by the parent custom prettier config.

--- admin/src/migrations/authentication-failures/1585297442338-AuthenticationFailuresMigration.ts
+++ admin/src/migrations/authentication-failures/1585297442338-AuthenticationFailuresMigration.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AuthenticationFailuresMigration1585297442338
-  implements MigrationInterface
-{
+export class AuthenticationFailuresMigration1585297442338 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
       `CREATE TABLE "authentication_failures" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "token" character varying, "authenticationAttemptedAt" TIMESTAMP NOT NULL, CONSTRAINT "PK_684fcb9924c8502d64b129cc8b1" PRIMARY KEY ("id"))`,
