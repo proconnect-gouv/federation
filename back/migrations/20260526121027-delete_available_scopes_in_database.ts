52ddfa7a6e4f12425595c62156aeb04fb7acdb62
BenoitSerrano <benoit.serrano10@gmail.com>
2026-05-26 13:26:55 +0100

    🔨 add script to delete scopes

---
+++ back/migrations/20260526121027-delete_available_scopes_in_database.ts
@@ -0,0 +1,15 @@
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ * @returns {Promise<void>}
+ */
+export const up = async (db, client) => {
+  await db.collection("scopes").deleteMany({});
+};
+
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ * @returns {Promise<void>}
+ */
+export const down = async (db, client) => {};
