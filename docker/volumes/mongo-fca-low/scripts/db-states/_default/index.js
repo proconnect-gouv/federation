d41525b100a9f53bb241bb132486813258178d0a
BenoitSerrano <benoit.serrano10@gmail.com>
2026-06-25 12:07:50 +0200

    🎨 style: format codebase with Prettier

--- docker/volumes/mongo-fca-low/scripts/db-states/_default/index.js
+++ docker/volumes/mongo-fca-low/scripts/db-states/_default/index.js
@@ -1,22 +1,22 @@
-print('Initializing SPs...');
-load('/opt/scripts/db-states/_default/sp.js');
+print("Initializing SPs...");
+load("/opt/scripts/db-states/_default/sp.js");
 
 /* ------------------------------------------------------------------------------- */
 
-print('Initializing IDPs...');
-load('/opt/scripts/db-states/_default/idp.js');
+print("Initializing IDPs...");
+load("/opt/scripts/db-states/_default/idp.js");
 
 /* ------------------------------------------------------------------------------- */
 
-print('Initializing FCA ACCOUNTS...');
-load('/opt/scripts/db-states/_default/account-fca.js');
+print("Initializing FCA ACCOUNTS...");
+load("/opt/scripts/db-states/_default/account-fca.js");
 
 /* ------------------------------------------------------------------------------- */
 
-print('Initializing Scopes...');
-load('/opt/scripts/db-states/_default/scopes.js');
+print("Initializing Scopes...");
+load("/opt/scripts/db-states/_default/scopes.js");
 
 /* ------------------------------------------------------------------------------- */
 
-print('Initializing Data providers...');
-load('/opt/scripts/db-states/_default/dp.js');
+print("Initializing Data providers...");
+load("/opt/scripts/db-states/_default/dp.js");
